d94ceb74e430bbe774ba9082a919ee7111a19aef
Buga Eusebiu Mihail <eusebiu.buga@gmail.com>
2020-11-06 13:25:58 +0100

    added 3d view and cubes

--- typescript codegolf/build/build.js
+++ typescript codegolf/build/build.js
@@ -43,19 +43,57 @@ var ColorHelper = (function () {
     };
     return ColorHelper;
 }());
+var x, y, z;
+function sin(x) {
+    return Math.sin(x);
+}
+var date = new Date();
+var time = date.getTime();
 var sketch = function (p) {
-    var x = 100;
-    var y = 200;
     p.setup = function () {
         var context = document.getElementById("processingInstance");
         var width = parseInt(context.style.width);
         var height = parseInt(context.style.height);
-        p.createCanvas(width, height);
+        p.createCanvas(width, height, "webgl");
     };
     p.draw = function () {
+        time = date.getTime();
         p.background(0);
-        p.fill(255);
-        p.rect(p.width / 2, p.height / 2, 50, 50);
+        var cubeSize = 10;
+        var offset = cubeSize / 2;
+        var t = p.millis() / 1000;
+        try {
+            var evalstring = '(x,y,z,t)=>'.toString().concat(document.getElementById("codeInput").value.toString());
+            var evaluatedFunction = eval(evalstring);
+        }
+        finally {
+        }
+        for (x = 0; x < 10; x++) {
+            for (y = 0; y < 10; y++) {
+                for (z = 0; z < 10; z++) {
+                    p.noStroke();
+                    p.fill(255);
+                    p.push();
+                    p.translate(x * (cubeSize + offset), y * (cubeSize + offset), z * (cubeSize + offset));
+                    var evalValue = 1;
+                    try {
+                        evalValue = evaluatedFunction();
+                    }
+                    catch (_a) {
+                        evalValue = 1;
+                    }
+                    var actualDimension = cubeSize * evalValue;
+                    if (actualDimension < 0) {
+                        p.fill(255, 0, 0, 10);
+                    }
+                    else {
+                        p.fill(255, 10);
+                    }
+                    p.box(actualDimension);
+                    p.pop();
+                }
+            }
+        }
     };
 };
 var processingInstance = new p5(sketch, "processingInstance");
--- typescript codegolf/sketch/sketch.ts
+++ typescript codegolf/sketch/sketch.ts
@@ -1,19 +1,60 @@
+let x, y, z;
+function sin(x: number) {
+  return Math.sin(x);
+}
+let date = new Date();
+let time = date.getTime();
 var sketch = (p: p5) => {
-  var x  = 100;
-  var y = 200;
-  
+
   p.setup = () => {
-    let context=document.getElementById("processingInstance");
-    let width:number=parseInt( context.style.width);
-    let height:number=parseInt( context.style.height);
-    p.createCanvas(width,height);
+    let context = document.getElementById("processingInstance");
+    let width: number = parseInt(context.style.width);
+    let height: number = parseInt(context.style.height);
+    p.createCanvas(width, height, "webgl");
   };
 
   p.draw = () => {
+    time = date.getTime();
     p.background(0);
-    p.fill(255);
-    p.rect(p.width/2,p.height/2, 50, 50);
+    let cubeSize = 10;
+    let offset = cubeSize / 2;
+    let t = p.millis() / 1000;
+    try {
+      let evalstring = '(x,y,z,t)=>'.toString().concat(document.getElementById("codeInput").value.toString());
+      let evaluatedFunction = eval(evalstring);
+    }
+    for (x = 0; x < 10; x++) {
+      for (y = 0; y < 10; y++) {
+        for (z = 0; z < 10; z++) {
+          p.noStroke();
+          p.fill(255);
+
+          p.push();
+
+          p.translate(x * (cubeSize + offset), y * (cubeSize + offset), z * (cubeSize + offset));
+          let evalValue = 1;
+          try {
+            evalValue = evaluatedFunction();
+          }
+          catch {
+            evalValue = 1;
+          }
+          let actualDimension = cubeSize * evalValue;
+
+          if (actualDimension < 0) {
+            p.fill(255, 0, 0, 10);
+          }
+          else {
+            p.fill(255, 10);
+          }
+          p.box(actualDimension);
+          p.pop();
+        }
+
+      }
+
+    }
   };
 };
 
-let processingInstance=new p5(sketch,"processingInstance");
+let processingInstance = new p5(sketch, "processingInstance");
